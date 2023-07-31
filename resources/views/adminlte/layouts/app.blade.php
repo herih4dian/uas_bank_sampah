1f1650383da8777f791cbe41cff24c64b7f3f57c
herih4dian <heri.hadian@selarastech.com>
2023-07-31 22:00:26 +0700

    Changes to be committed: 	modified:   resources/views/adminlte/layouts/app.blade.php

--- resources/views/adminlte/layouts/app.blade.php
+++ resources/views/adminlte/layouts/app.blade.php
@@ -122,37 +122,58 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+            <li class="nav-item {{ (request()->is('bank*')) ? 'menu-open' : '' }}"> <!-- menu-open -->
+              <a href="#" class="nav-link {{ (request()->is('bank*')) ? 'active' : '' }}"> <!-- active -->
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                Bank Sampah
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="{{ url('harga') }}" class="nav-link {{ (request()->is('harga')) ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>Harga Jual Sampah</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ url('jenis') }}" class="nav-link {{ (request()->is('jenis')) ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>Jenis Sampah</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ url('satuan') }}" class="nav-link {{ (request()->is('satuan')) ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Satuan</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          <li class="nav-item {{ (request()->is('manajemen*')) ? 'menu-open' : '' }}"> <!-- menu-open -->
+              <a href="#" class="nav-link {{ (request()->is('manajemen*')) ? 'active' : '' }}"> <!-- active -->
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Management Nasabah
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ url('nasabah') }}" class="nav-link {{ (request()->is('nasabah')) ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Nasabah</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ url('transaksi') }}" class="nav-link {{ (request()->is('transaksi')) ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Riwayat Transaksi</p>
                 </a>
               </li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
           </li>
         </ul>
       </nav>
