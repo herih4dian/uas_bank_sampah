272dfff612a3731f45cf7349d4b2fc7fb2d668f0
herih4dian <heri.hadian@selarastech.com>
2023-08-01 09:01:44 +0700

    Changes to be committed: 	modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
+            <h1 class="m-0">Home</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
+              <li class="breadcrumb-item active"></li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -26,62 +26,63 @@
     <div class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
+          <div class="col-lg-3 col-6">
+          
+          <div class="small-box bg-info">
+          <div class="inner">
+          <h3>150</h3>
+          <p>New Orders</p>
           </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+          <div class="icon">
+          <i class="ion ion-bag"></i>
           </div>
-          <!-- /.col-md-6 -->
-        </div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+          </div>
+          
+          <div class="col-lg-3 col-6">
+          
+          <div class="small-box bg-success">
+          <div class="inner">
+          <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <p>Bounce Rate</p>
+          </div>
+          <div class="icon">
+          <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+          </div>
+          
+          <div class="col-lg-3 col-6">
+          
+          <div class="small-box bg-warning">
+          <div class="inner">
+          <h3>44</h3>
+          <p>User Registrations</p>
+          </div>
+          <div class="icon">
+          <i class="ion ion-person-add"></i>
+          </div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+          </div>
+          
+          <div class="col-lg-3 col-6">
+          
+          <div class="small-box bg-danger">
+          <div class="inner">
+          <h3>65</h3>
+          <p>Unique Visitors</p>
+          </div>
+          <div class="icon">
+          <i class="ion ion-pie-graph"></i>
+          </div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+          </div>
+          
+          </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
